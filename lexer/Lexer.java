ed925df7c41ac5923b239fc549c8ef70529c6215
Joshua Lipstone <JoshuaLipstone@gmail.com>
2013-08-14 15:11:47 -0700

    Now properly supports descent tokens.

--- lexer/Lexer.java
+++ lexer/Lexer.java
@@ -1,6 +1,7 @@
 package lexer;
 
 import java.util.ArrayList;
+import java.util.Stack;
 import java.util.regex.Matcher;
 
 public class Lexer {
@@ -15,66 +16,65 @@ public class Lexer {
 	}
 	
 	public final Token lex(String input) {
+		Stack<Token> descendStack = new Stack<>();
 		Token head = new Token(), output = head;
 		while (input.length() > 0) {
-			System.out.println(head.getFirstToken());
 			Descender d = null;
 			for (Descender descender : descenders.getValues())
-				if (input.length() > descender.open.length() + descender.close.length() && input.substring(0, descender.open.length()).equals(descender.open)) {
+				if (input.length() >= descender.open.length() && input.substring(0, descender.open.length()).equals(descender.open)) {
 					d = descender;
 					break;
 				}
 			if (d != null) {
-				int end = getEndIndex(input, 0, d.open, d.close);
-				if (end > 0) {
-					head = head.append(new Token(d.apply(input.substring(1, end), this), d.getType()));
-					input = (end + d.close.length() == input.length()) ? "" : input.substring(end + d.close.length()).trim();
-					continue;
-				}
-				System.err.println("There was a Descender that did not have a matching close token.");
+				Token descent = new Token();
+				head = head.append(new Token(descent, d.getType()));
+				descendStack.push(head);
+				head = descent;
+				input = input.substring(d.open.length());
+				continue;
 			}
 			else {
-				Rule<?> hit = null;
-				String match = "";
-				Matcher m;
-				for (Rule<?> rule : rules.getValues()) {
-					m = rule.pattern.matcher(input);
-					if (!m.find() || m.start() != 0 || m.group().length() == 0)
+				for (Descender descender : descenders.getValues())
+					if (input.length() >= descender.close.length() && input.substring(0, descender.close.length()).equals(descender.close)) {
+						d = descender;
+						break;
+					}
+				if (d != null) {
+					if (descendStack.isEmpty() || descendStack.peek().getCarType() != d.getType())
+						System.err.println("There was an unbalanced descender in the input.");
+					else {
+						input = input.substring(d.close.length());
+						head = descendStack.pop();
 						continue;
-					hit = rule;
-					if (match.length() < m.group().length())
-						match = m.group();
-				}
-				if (hit != null) {
-					head = head.append(new Token(hit.apply(match, this), hit.getType()));
-					input = input.substring(match.length()).trim();
-					continue;
+					}
 				}
 				else {
-					System.err.println("There was an unrecognized character.");
+					Rule<?> hit = null;
+					String match = "";
+					Matcher m;
+					for (Rule<?> rule : rules.getValues()) {
+						m = rule.pattern.matcher(input);
+						if (!m.find() || m.start() != 0 || m.group().length() == 0)
+							continue;
+						hit = rule;
+						if (match.length() < m.group().length())
+							match = m.group();
+					}
+					if (hit != null) {
+						head = head.append(new Token(hit.apply(match, this), hit.getType()));
+						input = input.substring(match.length()).trim();
+						continue;
+					}
+					else {
+						System.err.println("There was an unrecognized character.");
+					}
 				}
-			}
-			//There was an error if this is reached
-			System.err.println("Remaining input: " + input);
-			break;
-		}
-		System.out.println(head);
-		return output;
-	}
-	
-	private final int getEndIndex(String section, int start, String startSymbol, String endSymbol) {
-		int index = -1, parenthesis = 0;
-		for (int i = start; i < section.length() - startSymbol.length() + 1 && i < section.length() - endSymbol.length() + 1; i++) {
-			if (section.substring(i, i + startSymbol.length()).equals(startSymbol))
-				parenthesis++;
-			if (section.substring(i, i + endSymbol.length()).equals(endSymbol))
-				parenthesis--;
-			if (parenthesis == 0) {
-				index = i;
+				//There was an error if this is reached
+				System.err.println("Remaining input: " + input);
 				break;
 			}
 		}
-		return index;
+		return output;
 	}
 	
 	public final void addRule(String name, Rule<?> rule) {
