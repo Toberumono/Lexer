6b9d66a8aa908eb556b16c501c34cc8f550c00a3
Joshua Lipstone <JoshuaLipstone@gmail.com>
2013-10-06 20:29:20 -0500

    Updated to sync the full project.

---
+++ src/testCases/TestSystem.java
@@ -0,0 +1,41 @@
+package testCases;
+
+import java.util.regex.Pattern;
+
+import lexer.Action;
+import lexer.Descender;
+import lexer.Lexer;
+import lexer.Rule;
+import lexer.Type;
+import lexer.errors.LexerException;
+
+public class TestSystem {
+	
+	public static void main(String[] args) {
+		Lexer lexer = new Lexer();
+		Type<Integer> integer = new Type<>("Integer");
+		Type<Double> decimal = new Type<>("Decimal");
+		lexer.addRule("Integer", new Rule<Integer>(Pattern.compile("[0-9]+"), integer, new Action<Integer>() {
+			@Override
+			public Integer action(String input, Lexer lexer) {
+				return new Integer(input);
+			}
+		}));
+		lexer.addRule("Decimal", new Rule<Double>(Pattern.compile("([0-9]+\\.[0-9]*|[0-9]*\\.[0-9]+)"), decimal, new Action<Double>() {
+			@Override
+			public Double action(String input, Lexer lexer) {
+				return new Double(input);
+			}
+		}));
+		lexer.addDescender("Parentheses", new Descender("(", ")", Type.TOKEN, null));
+		lexer.addDescender("Brackets", new Descender("[", "]", Type.TOKEN, null));
+		String test = "10.0 100 (3.0 300) [51 5 6] ()";
+		try {
+			System.out.println(test + " -> " + lexer.lex(test));
+		}
+		catch (LexerException e) {
+			System.out.println(test + " -> " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
